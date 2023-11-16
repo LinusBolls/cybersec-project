2e5dc2971bf85490cf744e2ef5e7c24a6a8569d8
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2023-11-17 00:10:11 +0100

    chore: error handling for /api/boxes

--- app/api/boxes/route.ts
+++ app/api/boxes/route.ts
@@ -1,21 +1,38 @@
+import Env from "@/env";
 import { verifyAccessToken } from "@/services/authService";
 import { createBox, getBoxes } from "@/services/boxService";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
 
-    const boxes = await getBoxes({ state: "PUBLISHED" });
+    try {
+        const boxes = await getBoxes({ state: "PUBLISHED" });
 
-    return NextResponse.json(boxes, { status: 200 });
+        return NextResponse.json(boxes, { status: 200 });
+
+    } catch (err) {
+
+        const message = (err instanceof Error && Env.verboseLogging) ? (err.message || "Unknown error") : "Unknown error";
+
+        return NextResponse.json({ message }, { status: 400 });
+    }
 }
 
 export async function POST(req: NextRequest) {
 
-    const accessToken = req.cookies.get("access_token")?.value;
+    try {
+        const accessToken = req.cookies.get("access_token")?.value;
 
-    const user = verifyAccessToken(accessToken!);
+        const user = verifyAccessToken(accessToken!);
 
-    const box = await createBox(user.sub, []);
+        const box = await createBox(user.sub, []);
 
-    return NextResponse.json(box, { status: 201 });
+        return NextResponse.json(box, { status: 201 });
+
+    } catch (err) {
+
+        const message = (err instanceof Error && Env.verboseLogging) ? (err.message || "Unknown error") : "Unknown error";
+
+        return NextResponse.json({ message }, { status: 400 });
+    }
 } 3
