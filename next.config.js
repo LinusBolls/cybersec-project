43fb7fadab23d7ae937626382828fe5be249a338
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2023-11-16 23:39:24 +0100

    security: security header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    /**
+     * since we don't interact with any other domains other than our own api,
+     * we can set or CORS and CSP to be as strict as possible.
+     * 
+     * TODO: tighten CSP
+     */
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "same-origin" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+                    // { key: "Content-Security-Policy", value: "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'; connect-src 'self'; font-src 'self'; frame-src 'self';" },
+                    { key: "X-Content-Type-Options", value: "nosniff" },
+                    { key: "X-Frame-Options", value: "SAMEORIGIN" },
+                    { key: "Referrer-Policy", value: "no-referrer" },
+                    { key: "X-XSS-Protection", value: "1; mode=block" },
+                ]
+            }
+        ]
+    }
+}
 module.exports = nextConfig
