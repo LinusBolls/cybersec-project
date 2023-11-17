5e3ecf5f3da880c0d52a332e9f83e17f0597bd5a
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2023-11-17 10:39:29 +0100

    feat: user feedback for LoginForm and RegistrationForm

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -9,8 +9,11 @@ import {
 } from "@material-tailwind/react";
 import { Formik, Form, Field } from 'formik';
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export function LoginForm() {
+
+  const router = useRouter();
 
   return <div className="flex items-center justify-center w-screen h-screen">
     <Formik
@@ -18,27 +21,34 @@ export function LoginForm() {
         email: '',
         password: '',
       }}
-      onSubmit={async (values) => {
+      onSubmit={async (values, { setFieldError }) => {
         const registerRes = await fetch("/api/auth/login", {
           method: "POST",
           body: JSON.stringify(values),
         });
-        const registerData = await registerRes.json();
+        const loginData = await registerRes.json();
 
-        alert(JSON.stringify(registerData, null, 2));
+        if (loginData.ok) {
+          router.push("/");
+        } else {
+          setFieldError('general', loginData.message);
+        }
       }}>
-
-      <Form className="md:w-48 lg:w-1/4">
+      {({ errors }) => (
+        <Form className="md:w-48 lg:w-1/4">
         <Card color="transparent" shadow={false}>
           <Typography variant="h4" color="blue-gray">
             Sign In
           </Typography>
-          <Typography color="gray" className="mt-1 font-normal">
+          <Typography color="gray" className="mt-1 font-normal mb-2">
             Welcome back!
           </Typography>
+            {errors.general && <Typography variant="h6" className="text-red-600">
+              {errors.general}
+            </Typography>}
           <div className="mb-1 flex flex-col gap-6">
             <label htmlFor="email">
-              <Typography variant="h6" color="blue-gray" className="-mb-3">
+                <Typography variant="h6" color="blue-gray" className="-mb-3">
                 Your Email
               </Typography>
             </label>
@@ -96,6 +106,7 @@ export function LoginForm() {
           </Typography>
         </Card>
       </Form>
+      )}
     </Formik>
   </div >
 }
--- components/RegistrationForm.tsx
+++ components/RegistrationForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from 'next/navigation';
 import {
   Card,
   Input,
@@ -12,6 +13,8 @@ import Link from "next/link";
 
 export function RegistrationForm() {
 
+  const router = useRouter();
+
   return <div className="flex items-center justify-center w-screen h-screen">
     <Formik
       initialValues={{
@@ -19,107 +22,115 @@ export function RegistrationForm() {
         name: '',
         password: '',
       }}
-      onSubmit={async (values) => {
+      onSubmit={async (values, { setFieldError }) => {
         const registerRes = await fetch("/api/auth/register", {
           method: "POST",
           body: JSON.stringify(values),
         });
         const registerData = await registerRes.json();
 
-        alert(JSON.stringify(registerData, null, 2));
+        if (registerData.ok) {
+          router.push("/");
+        } else {
+          setFieldError('general', registerData.message);
+        }
       }}>
-
-      <Form className="md:w-48 lg:w-1/4">
-        <Card color="transparent" shadow={false}>
-          <Typography variant="h4" color="blue-gray">
-            Sign Up
-          </Typography>
-          <Typography color="gray" className="mt-1 font-normal">
-            Nice to meet you! Enter your details to register.
-          </Typography>
-          <div className="mb-1 flex flex-col gap-6">
-            <label htmlFor="name">
-              <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Your Name
-              </Typography>
-            </label>
-            <Field
-              type="name"
-              as={Input}
+      {({ errors }) => (
+        <Form className="md:w-48 lg:w-1/4">
+          <Card color="transparent" shadow={false}>
+            <Typography variant="h4" color="blue-gray">
+              Sign Up
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal mb-2">
+              Nice to meet you! Enter your details to register.
+            </Typography>
+            {errors.general && <Typography variant="h6" className="text-red-600">
+              {errors.general}
+            </Typography>}
+            <div className="mb-1 flex flex-col gap-6">
+              <label htmlFor="name">
+                <Typography variant="h6" color="blue-gray" className="-mb-3">
+                  Your Name
+                </Typography>
+              </label>
+              <Field
+                type="name"
+                as={Input}
+                crossOrigin="anonymous"
+                id="name"
+                name="name"
+                placeholder="Your name"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 placeholder:text-gray-700"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+              />
+              <label htmlFor="email">
+                <Typography variant="h6" color="blue-gray" className="-mb-3">
+                  Your Email
+                </Typography>
+              </label>
+              <Field
+                as={Input}
+                crossOrigin="anonymous"
+                id="email"
+                name="email"
+                placeholder="Your email"
+                type="email"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 placeholder:text-gray-700"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+              />
+              <label htmlFor="password">
+                <Typography variant="h6" color="blue-gray" className="-mb-3">
+                  Password
+                </Typography>
+              </label>
+              <Field
+                type="password"
+                as={Input}
+                crossOrigin="anonymous"
+                id="password"
+                name="password"
+                placeholder="Your password"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 placeholder:text-gray-700"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+              />
+            </div>
+            {/* <Checkbox
               crossOrigin="anonymous"
-              id="name"
-              name="name"
-              placeholder="Your name"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 placeholder:text-gray-700"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-            <label htmlFor="email">
-              <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Your Email
-              </Typography>
-            </label>
-            <Field
-              as={Input}
-              crossOrigin="anonymous"
-              id="email"
-              name="email"
-              placeholder="Your email"
-              type="email"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 placeholder:text-gray-700"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-            <label htmlFor="password">
-              <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Password
-              </Typography>
-            </label>
-            <Field
-              type="password"
-              as={Input}
-              crossOrigin="anonymous"
-              id="password"
-              name="password"
-              placeholder="Your password"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 placeholder:text-gray-700"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-          </div>
-          {/* <Checkbox
-            crossOrigin="anonymous"
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center font-normal"
-              >
-                I agree the
-                <a
-                  href="#"
-                  className="font-medium transition-colors hover:text-gray-900"
+              label={
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="flex items-center font-normal"
                 >
-                  &nbsp;Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          /> */}
-          <Button className="mt-6" fullWidth type="submit">
-            Sign up
-          </Button>
-          <Typography color="gray" className="mt-4 text-center font-normal">
-            Already have an account?{" "}
-            <Link href="/login" className="font-medium text-gray-900">
-              Sign In
-            </Link>
-          </Typography>
-        </Card>
-      </Form>
+                  I agree the
+                  <a
+                    href="#"
+                    className="font-medium transition-colors hover:text-gray-900"
+                  >
+                    &nbsp;Terms and Conditions
+                  </a>
+                </Typography>
+              }
+              containerProps={{ className: "-ml-2.5" }}
+            /> */}
+            <Button className="mt-6" fullWidth type="submit">
+              Sign up
+            </Button>
+            <Typography color="gray" className="mt-4 text-center font-normal">
+              Already have an account?{" "}
+              <Link href="/login" className="font-medium text-gray-900">
+                Sign In
+              </Link>
+            </Typography>
+          </Card>
+        </Form>
+      )}
     </Formik>
   </div >
 }
